ca252398aa9f29cc51f3c0dbd7725d3976b6a076
Pelemo Olorunshola <pelemovictor0@gmail.com>
2019-01-30 01:13:39 +0100

    Add project files.

---
+++ CardPayment/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CardPayment.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult CardPayment()
+        {
+            Random r = new Random();
+            ViewBag.TID="Ercas"+r.Next(1, 200000);
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ CardPayment/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CardPayment.MvcApplication" Language="C#" %>
